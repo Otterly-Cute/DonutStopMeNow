66ea1c7f2d29a1cd00ad67e0dafbd7c05abde2f8
Otterly-Cute <144897039+Otterly-Cute@users.noreply.github.com>
2024-12-07 21:30:48 +0100

    shit doesnt work....

--- DonutStopMeNow/Assets/Scripts/HoleManager.cs
+++ DonutStopMeNow/Assets/Scripts/HoleManager.cs
@@ -7,13 +7,27 @@ public class HoleManager : MonoBehaviour
     [SerializeField] private Image circleImg;
     [SerializeField] private Transform holeGameObj;
 
-    void Start()
+    private void ProgressBar (int number)
     {
-        
+        circleCapacity = 1f / number;
+        circleImg.fillAmount += circleCapacity;
+
+        if (circleImg.fillAmount.Equals(1f))
+        {
+            holeGameObj.localScale += new Vector3(x: 0.3f, y: 0f, z: 0.3f);
+
+            circleImg.fillAmount = 0f;
+        }
     }
 
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("collectable"))
+        {
+            ProgressBar(number: 20);
+
+            other.gameObject.SetActive(false);
+        }
     }
+
 }
